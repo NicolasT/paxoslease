dcc823211216ad8565f4b08cccbb92f3d38674c3
Nicolas Trangez <ikke@nicolast.be>
2016-09-13 00:04:40 +0200

    indexed-prelude: Add `ifThenElse`
    For `RebindableSyntax` to work properly (i.e. support
    `if ... then ... else ...`), one needs a binding for `ifThenElse` in scope.
    
    This commit adds the most basic implementation to the 'indexed' `Prelude`.

--- indexed-prelude/src/Control/Monad/Indexed/Prelude.hs
+++ indexed-prelude/src/Control/Monad/Indexed/Prelude.hs
@@ -11,7 +11,7 @@ use standard @do@-notation in the context of /indexed/ monads.
 
 module Control.Monad.Indexed.Prelude (
     -- * Support for @RebindableSyntax@ (with 'fail' = 'error')
-    (>>), (>>=), fail,
+    (>>), (>>=), fail, ifThenElse,
     -- * 'Functor', 'Applicative' and 'Monad'-like operators
     (<$>), (<$), (<*>), (<*), (*>), pure, (=<<), return,
     -- * Re-exports
@@ -42,6 +42,11 @@ a >> b = a >>= const b
 fail :: String -> m i j a
 fail s = error $ "fail: " ++ s
 {-# INLINE fail #-}
+
+-- | Standard implementation for conditional constructs.
+ifThenElse :: Bool -> a -> a -> a
+ifThenElse b a1 a2 = if b then a1 else a2
+{-# INLINE ifThenElse #-}
 
 -- | Indexed version of 'Prelude.<$>'.
 (<$>) :: IxFunctor f => (a -> b) -> f i j a -> f i j b
